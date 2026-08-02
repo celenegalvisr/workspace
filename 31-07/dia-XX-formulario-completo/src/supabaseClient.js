64bd247f22d8e28229e15e158aa78c58dac6e437
celenegalvisr <celenegalvisr@ielacandelariamedellin.edu.co>
2026-08-02 03:54:40 +0000

    dia XX: formulario completo

--- 31-07/dia-XX-formulario-completo/src/supabaseClient.js
+++ 31-07/dia-XX-formulario-completo/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = https://yzfahymnfmuxpqsnxkqo.supabase.co
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 // DIAGNÓSTICO: Esto te dirá en la consola de tu navegador si las llaves se están leyendo
