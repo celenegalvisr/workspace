239bd4a9663cd8bd6b27cd8f6affda64c1ab0eaf
celenegalvisr <celenegalvisr@ielacandelariamedellin.edu.co>
2026-08-02 03:53:12 +0000

    dia XX: formulario completo

--- 31-07/dia-XX-formulario-completo/src/supabaseClient.js
+++ 31-07/dia-XX-formulario-completo/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseUrl = https://yzfahymnfmuxpqsnxkqo.supabase.co
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 // DIAGNÓSTICO: Esto te dirá en la consola de tu navegador si las llaves se están leyendo
@@ -11,4 +11,4 @@ if (!supabaseUrl || !supabaseAnonKey) {
     console.error("❌ Error crítico: Las credenciales de Supabase no están configuradas en el archivo .env");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
